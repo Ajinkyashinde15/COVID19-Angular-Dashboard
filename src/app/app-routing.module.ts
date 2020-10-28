7ff4740c8574453d86d363c2c2fc727b7cb00720
Ajinkya Shinde <ajinkya.shinde@autoaddress.ie>
2020-10-28 08:45:58 +0000

    Basic skeleton, modules, components created

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DefaultComponent } from './layout/default/default.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { PostsComponent } from './modules/posts/posts.component';
+
+const routes: Routes = [{
+
+  path : '',
+  component : DefaultComponent,
+  children : [{
+    path : '',
+    component : DashboardComponent
+  },
+  {
+    path : 'posts',
+    component : PostsComponent
+  }]
+}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../shared/components/header/header.component';
+import { SidebarComponent } from '../shared/components/sidebar/sidebar.component';
+import { FooterComponent } from '../shared/components/footer/footer.component';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
+  ],
+  imports: [
+    CommonModule,
+    MatDividerModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatListModule,
+    MatSidenavModule,
+    FlexLayoutModule,
+    RouterModule
+  ],
+  exports : [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
+  ]
+})
+export class SharedModule { }
